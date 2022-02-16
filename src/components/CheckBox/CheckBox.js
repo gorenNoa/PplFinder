385b7ffddea2c268deb7becd5bc386f7d78a285f
Noa <noagoren@post.bgu.ac.il>
2022-02-16 18:09:22 +0200

    checkbox sends value to function

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -2,10 +2,14 @@ import React from "react";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import * as S from "./style";
+import { usePeopleFetch } from "hooks";
+import axios from "axios";
+
 
 const CheckBox = ({ isChecked, onChange, label, value }) => {
   const handleChange = () => {
-    onChange && usePeopleFetchFromCountry(value);
+    fetchUsersFromCountry(value);
+    onChange && onChange(value);
   };
   return (
     <S.CheckBox>
@@ -16,5 +20,12 @@ const CheckBox = ({ isChecked, onChange, label, value }) => {
     </S.CheckBox>
   );
 };
-
+async function fetchUsersFromCountry(value) {
+  // setIsLoading(true);
+  console.log(value);
+  const response = await axios.get(`https://randomuser.me/api/?nat=${value}`);
+  console.log(response.data.results);
+  // setIsLoading(false);
+  // setUsers(response.data.results);
+}
 export default CheckBox;
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,1 @@
 export { usePeopleFetch } from "./usePeopleFetch";
-export { usePeopleFetchFromCountry } from "./usePeopleFetch";
